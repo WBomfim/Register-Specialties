c2cb2a452185216274c8ebc975cfeb51488bea2a
WBomfim <willianbomfim@hotmail.com>
2023-03-17 15:17:39 -0300

    feat: Implementado a classe Service ProfessionalService

--- src/main/java/app/trybe/specialityapp/service/ProfessionalService.java
+++ src/main/java/app/trybe/specialityapp/service/ProfessionalService.java
@@ -1,5 +1,34 @@
 package app.trybe.specialityapp.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import app.trybe.specialityapp.model.Professional;
+import app.trybe.specialityapp.repository.ProfessionalRepository;
+
+@Service
 public class ProfessionalService {
-  
+  @Autowired
+  private ProfessionalRepository professionalRepository;
+
+  public List<Professional> findAll() {
+    return professionalRepository.findAll();
+  }
+
+  public Professional findById(Integer id) {
+    return professionalRepository.findById(id).get();
+  }
+
+  public Professional add(Professional professional) {
+    return professionalRepository.save(professional);
+  }
+
+  public Professional update(Professional professional) {
+    return professionalRepository.save(professional);
+  }
+
+  public void delete(Integer id) {
+    professionalRepository.deleteById(id);
+  }
 }
